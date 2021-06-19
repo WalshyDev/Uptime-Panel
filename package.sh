4d3b6c7cd30dac23678c67c97492ff8d424fb90a
Daniel Walsh <walshydev@gmail.com>
2021-06-19 19:45:26 +0100

    Add images

--- package.sh
+++ package.sh
@@ -1,9 +1,15 @@
 VERSION=$(cat package.json | jq -r .version)
 
+# Cleanup previous runs
+rm -rf dist walshydev-uptime-panel*
+
+# Build
 yarn install --pure-lockfile
 yarn build
 
+# Sign
 #npx @grafana/toolkit plugin:sign
 
-mv dist/ walshydev-uptime
-zip -r "walshydev-uptime-$VERSION.zip" walshydev-uptime
+# Package
+mv dist/ walshydev-uptime-panel
+zip -r "walshydev-uptime-panel-$VERSION.zip" walshydev-uptime-panel
